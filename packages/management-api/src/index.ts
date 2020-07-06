3a655b2d9aad0969302ecb189052fd42297dbcaa
Patrick Holloway <patrickjholloway@gmail.com>
2020-07-06 15:06:43 -0400

    knex objection spike

--- packages/management-api/src/index.ts
+++ packages/management-api/src/index.ts
@@ -1,11 +1,12 @@
 import { PORT } from 'src/configuration';
 import { getApp } from 'src/app';
+import getDb from 'src/db';
 
 const startServer = () => {
   try {
     const app = getApp();
-
-    app.listen(PORT, () => {
+    app.listen(PORT, async ()  => {
+      const db = await getDb();
       console.log(`server started at http://localhost:${PORT}`);
     });
   } catch (error) {
